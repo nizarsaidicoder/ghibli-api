3dc8a082d15fd20bdc70122ebcaaaf31dd11f997
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-08-06 13:55:07 +0200

    Implemented merging the movie title with the character info

--- routes/characters.js
+++ routes/characters.js
@@ -1,65 +1,66 @@
 const express = require("express");
 const router = express.Router();
 const Character = require("../models/Character");
+const Movie = require("../models/movie");
 
 const filter = "char_id name image movie";
-
-// Get character based on name using ?name= query
-router.get("/", async (req, res) => {
-  try {
-    const nameQuery = req.query.name;
-    if (!nameQuery) {
-      try {
-        const characters = await Character.find().select(filter);
-        return res.json(characters);
-      } catch (err) {
-        return res.status(500).json({ message: err.message });
-      }
-    } else if (nameQuery.length < 3) {
-      return res
-        .status(400)
-        .json({ message: "Name must be at least 3 characters" });
-    }
-    const characters = await Character.find({
-      name: { $regex: nameQuery, $options: "i" }, // 'i' for case-insensitive
-    }).select(filter);
-    res.json(characters);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Get all characters from a movie
-router.get("/movie/:id", async (req, res) => {
-  try {
-    const characters = await Character.find({ movie: req.params.id }).select(
-      filter
-    );
-    res.json(characters);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
 // Get one character
 router.get("/:id", getCharacter, (req, res) => {
   res.json(res.character);
 });
 
+// Get all characters
+router.get("/", async (req, res) => {
+  try {
+    const nameQuery = req.query.name;
+    const movieQuery = req.query.movie;
+    const idQuery = req.query.id;
+    const queryFilter = {};
+    if (nameQuery) {
+      if (nameQuery.length < 3) {
+        return res
+          .status(400)
+          .json({ message: "Name must be at least 3 characters" });
+      }
+      queryFilter.name = { $regex: nameQuery, $options: "i" };
+    }
+    if (movieQuery) {
+      queryFilter.movie = movieQuery;
+    }
+    if (idQuery) {
+      queryFilter.char_id = idQuery;
+    }
+    const characters = await Character.find(queryFilter).select(filter);
+    res.json(characters);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 async function getCharacter(req, res, next) {
   let character;
   try {
-    character = await Character.find({ char_id: req.params.id });
+    character = await Character.findOne({ char_id: req.params.id }, { _id: 0 });
     if (character == null) {
       return res.status(404).json({ message: "Cannot find character" });
     }
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
-  res.character = character;
-  next();
+  // get movie name
+  try {
+    const mov = await Movie.findOne(
+      { id: character.movie },
+      { title: 1, _id: 0 }
+    );
+    res.character = {
+      ...character._doc,
+      movie: { id: character.movie, title: mov.title },
+    };
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 }
-// Get all characters
-router.get("/", async (req, res) => {});
 
 module.exports = router;
