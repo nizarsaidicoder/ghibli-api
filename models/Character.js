e305c73635ec92fd52ec902573f461ac3dc248f6
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-08-09 15:25:37 +0200

    modified the collection name in models

--- models/Character.js
+++ models/Character.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
-const characterSchema = new mongoose.Schema({
-  char_id: {
-    type: Number,
-    required: true,
+const characterSchema = new mongoose.Schema(
+  {
+    char_id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    movie: {
+      type: Number,
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  movie: {
-    type: Number,
-    required: true,
-  },
-});
+  { collection: "characters" }
+);
 module.exports = mongoose.model("Character", characterSchema, "characters");
--- models/movie.js
+++ models/movie.js
@@ -1,44 +1,47 @@
 const mongoose = require("mongoose");
-const movieSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
+const movieSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    release_date: {
+      type: Number,
+      required: true,
+    },
+    running_time: {
+      type: String,
+      required: true,
+    },
+    producer: {
+      type: String,
+      required: true,
+    },
+    director: {
+      type: String,
+      required: true,
+    },
+    synopsis: {
+      type: String,
+      required: true,
+    },
+    imdb_rating: {
+      type: Number,
+      required: true,
+    },
+    metascore: {
+      type: Number,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  release_date: {
-    type: Number,
-    required: true,
-  },
-  running_time: {
-    type: String,
-    required: true,
-  },
-  producer: {
-    type: String,
-    required: true,
-  },
-  director: {
-    type: String,
-    required: true,
-  },
-  synopsis: {
-    type: String,
-    required: true,
-  },
-  imdb_rating: {
-    type: Number,
-    required: true,
-  },
-  metascore: {
-    type: Number,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-  },
-});
+  { collection: "movies" }
+);
 module.exports = mongoose.model("Movie", movieSchema, "movies");
